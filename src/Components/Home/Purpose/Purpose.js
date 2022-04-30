5540493bc614eb1dfb49e724cc7d88aeca19e7c2
ADSM <yesform2018@gmail.com>
2022-04-30 07:09:29 +0600

    firebase deploy completed

--- src/Components/Home/Purpose/Purpose.js
+++ src/Components/Home/Purpose/Purpose.js
@@ -3,13 +3,17 @@ import React from 'react';
 const Purpose = () => {
     return (
         <div className='grid md:grid-cols-2 text-white'>
-            <div className='bg-[#FF5400] bg-no-repeat bg-left p-20'>
-                <h3 className='text-2xl'>ARE YOU LOOKING FOR A CAR?</h3>
-                <p>Search your car in our Inventory and request a quote on the vehicle of your choosing.</p>
+            <div className='bg-purpose-bg1 bg-no-repeat bg-left'>
+                <div className='bg-[#FF5400] opacity-80 p-20'>
+                    <h3 className='text-2xl'>ARE YOU LOOKING FOR A CAR?</h3>
+                    <p>Search your car in our Inventory and request a quote on the vehicle of your choosing.</p>
+                </div>
             </div>
-            <div className='bg-gray-800 bg-no-repeat bg-right p-20'>
-                <h3 className='text-2xl'>DO YOU WANT TO SELL YOUR CAR?</h3>
-                <p>Would you like to sell your car? Let us help you with professional marketer.</p>
+            <div className='bg-purpose-bg2 bg-no-repeat bg-right'>
+                <div className='bg-gray-800 opacity-80 p-20'>
+                    <h3 className='text-2xl'>DO YOU WANT TO SELL YOUR CAR?</h3>
+                    <p>Would you like to sell your car? Let us help you with professional marketer.</p>
+                </div>
             </div>
         </div>
     );
