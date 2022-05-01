cb101a4c6eecb872ee92cefa951ef3adaf68f8db
ADSM <yesform2018@gmail.com>
2022-05-01 06:22:33 +0600

    manage inventory page completed

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
 import google from '../../images/icon/google.png';
 import facebook from '../../images/icon/facebook.png';
--- src/Components/ManageInventory/InventoryItems/InventoryItems.js
+++ src/Components/ManageInventory/InventoryItems/InventoryItems.js
@@ -1,9 +1,68 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import useCar from '../../Hook/useCar/useCar';
 
 const InventoryItems = () => {
+
+    const [cars] = useCar();
+    const navigate = useNavigate();
+
     return (
-        <div>
-            inventory
+        <div className="max-w-7xl mx-auto mt-10">
+            <div className="flex flex-col">
+                <div className="overflow-x-auto shadow-md sm:rounded-lg">
+                    <div className="inline-block min-w-full align-middle">
+                        <div className="overflow-hidden ">
+                            <table className="min-w-full divide-y divide-gray-200 table-fixed dark:divide-gray-700">
+                                <thead className="bg-gray-100 dark:bg-gray-700">
+                                    <tr>
+                                        <th scope="col" className="py-3 px-6 text-xs font-medium tracking-wider text-left text-gray-700 uppercase dark:text-gray-400">
+                                            Image
+                                        </th>
+                                        <th scope="col" className="py-3 px-6 text-xs font-medium tracking-wider text-left text-gray-700 uppercase dark:text-gray-400">
+                                            Product Name
+                                        </th>
+                                        <th scope="col" className="py-3 px-6 text-xs font-medium tracking-wider text-left text-gray-700 uppercase dark:text-gray-400">
+                                            Suplier
+                                        </th>
+                                        <th scope="col" className="py-3 px-6 text-xs font-medium tracking-wider text-left text-gray-700 uppercase dark:text-gray-400">
+                                            Price
+                                        </th>
+                                        <th scope="col" className="py-3 px-6 text-xs font-medium tracking-wider text-left text-gray-700 uppercase dark:text-gray-400">
+                                            Edit
+                                        </th>
+                                        <th scope="col" className="py-3 px-6 text-xs font-medium tracking-wider text-left text-gray-700 uppercase dark:text-gray-400">
+                                            Delete
+                                        </th>
+                                    </tr>
+                                </thead>
+                                <tbody className="bg-white divide-y divide-gray-200 dark:bg-gray-800 dark:divide-gray-700">
+                                    {
+                                        cars.map(car => {
+                                            return <tr key={car._id} className="hover:bg-gray-100 dark:hover:bg-gray-700">
+                                                <td className="p-4 w-4">
+                                                    <div className="flex items-center">
+                                                        <img src={car.image} alt="" />
+                                                    </div>
+                                                </td>
+                                                <td className="py-4 px-6 text-sm font-medium text-gray-900 whitespace-nowrap dark:text-white">{car.name}</td>
+                                                <td className="py-4 px-6 text-sm font-medium text-gray-500 whitespace-nowrap dark:text-white">{car.suplier}</td>
+                                                <td className="py-4 px-6 text-sm font-medium text-gray-900 whitespace-nowrap dark:text-white">${car.price}</td>
+                                                <td className="py-4 px-6 text-sm font-medium text-right whitespace-nowrap">
+                                                    <button onClick={() => navigate(`/manageInventory/${car._id}`)} className="text-blue-600 dark:text-blue-500 hover:underline">Edit</button>
+                                                </td>
+                                                <td className="py-4 px-6 text-sm font-medium text-right whitespace-nowrap">
+                                                    <button className="text-red-600 dark:text-red-500 hover:underline">Delete</button>
+                                                </td>
+                                            </tr>
+                                        })
+                                    }
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
